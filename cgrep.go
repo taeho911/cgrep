88331376e9baad61f03fbfff654e5c8542b831b4
Taeho <kimdogg911@gmail.com>
2021-11-20 17:00:31 +0900

    Added comment and korean doc

--- cgrep.go
+++ cgrep.go
@@ -17,18 +17,29 @@ import (
 	"golang.org/x/text/transform"
 )
 
+// Regular expressions to match pattern
 type regexArray []string
+
+// Regular expressions not to match pattern
 type invertArray []regexp.Regexp
+
+// Directories to skip searching
 type skipDirArray []string
+
+// Grepped lines' contents and line numbers
 type grep struct {
 	lineNum int
 	line    string
 }
+
+// Grepped results for each file
 type grepStruct struct {
 	file  string
 	greps []grep
 }
 
+// Overriding functions to take options' arguments as array
+// --------------------------------------------------------
 func (ra *regexArray) String() string {
 	return ""
 }
@@ -56,13 +67,18 @@ func (sa *skipDirArray) Set(val string) error {
 	return nil
 }
 
+// --------------------------------------------------------
+
 func main() {
 	var ra regexArray
 	var ia invertArray
 	var sa skipDirArray
 	var dirs []string
 	encs := []string{"utf8", "sjis", "encjp", "iso2022jp", "enckr"}
+	// In case of encountering .git and .svn directories, cgrep will skip those directories by default
 	defaultSkipDirs := []string{".git", ".svn"}
+
+	// Setting available flags
 	flag.Var(&ra, "e", "Regex")
 	flag.Var(&ia, "v", "Invert match")
 	flag.Var(&sa, "s", "Skip dir")
@@ -72,8 +88,14 @@ func main() {
 	enc := flag.String("enc", encs[0], fmt.Sprintf("Encoding %v", encs))
 	filenameOnlyFlag := flag.Bool("f", false, "Print filename only")
 	flag.Parse()
+
+	// Rest of arguments after parsing flags
 	tail := flag.Args()
+
+	// Validate arguments
+	// When there is no -e option, cgrep guess the tail would be the pattern to match
 	if len(ra) == 0 {
+		// If there is no -e options and no tail, cgrep will exit program because there is nothing to search
 		if len(tail) < 1 {
 			fmt.Fprintf(os.Stderr, "Args not enough => len(args) = %d\n", len(tail))
 			os.Exit(1)
@@ -83,13 +105,16 @@ func main() {
 	} else {
 		dirs = tail[:]
 	}
+	// Exit program because there is no pattern to match
 	if len(ra) < 1 {
 		fmt.Fprintf(os.Stderr, "No regex => len(regex) = %d\n", len(ra))
 		os.Exit(1)
 	}
+	// If there is no designated searching target directory, cgrep search from current directory by default
 	if len(dirs) < 1 {
 		dirs = append(dirs, ".")
 	}
+	// if all flag is false, append default skip directories (.git, .svn)
 	if !(*allFlag) {
 		for i := 0; i < len(defaultSkipDirs); i++ {
 			sa = append(sa, defaultSkipDirs[i])
@@ -98,11 +123,13 @@ func main() {
 	var cra []regexp.Regexp
 	for i := 0; i < len(ra); i++ {
 		if *caseInsensitiveFlag {
+			// In case of case insensitive matching, add (?i) regex
 			cra = append(cra, *regexp.MustCompile("(?i)" + ra[i]))
 		} else {
 			cra = append(cra, *regexp.MustCompile(ra[i]))
 		}
 	}
+	// Set encoding method according to --enc option
 	var encoding encoding.Encoding
 	switch *enc {
 	case encs[0]:
@@ -130,7 +157,9 @@ func grepContents(
 	encoding encoding.Encoding,
 	filenameOnlyFlag bool,
 ) {
+	// Make wait group to control goroutine
 	var wg sync.WaitGroup
+	// Walk through searching target dirs
 	for i := 0; i < len(dirs); i++ {
 		err := filepath.Walk(dirs[i], func(path string, info os.FileInfo, err error) error {
 			skipDir := false
@@ -138,6 +167,7 @@ func grepContents(
 				return filepath.SkipDir
 			}
 			fileName := info.Name()
+			// If the name of dir is in skip dirs list, skip searching
 			for i := 0; i < len(sa); i++ {
 				if fileName == sa[i] {
 					skipDir = true
@@ -147,11 +177,14 @@ func grepContents(
 			if skipDir {
 				return filepath.SkipDir
 			}
+			// In case of file
 			if !info.IsDir() {
+				// Concurrent searching via goroutine
 				if concFlag {
 					wg.Add(1)
 					go grepWorkConc(path, cra, ia, encoding, &wg, filenameOnlyFlag)
 				} else {
+					// Normal searching
 					grepWork(path, cra, ia, encoding, filenameOnlyFlag)
 				}
 			}
@@ -162,9 +195,11 @@ func grepContents(
 			os.Exit(1)
 		}
 	}
+	// Block exit until all goroutine is closed
 	wg.Wait()
 }
 
+// Actually grepping function for concurrent searching
 func grepWorkConc(
 	file string,
 	cra []regexp.Regexp,
@@ -174,6 +209,7 @@ func grepWorkConc(
 	filenameOnlyFlag bool,
 ) {
 	defer wg.Done()
+	// Open and read the target file
 	fp, err := os.Open(file)
 	if err != nil {
 		return
@@ -183,16 +219,20 @@ func grepWorkConc(
 	scanner := bufio.NewScanner(reader)
 	lineNum := 0
 	var greps []grep
+	// Scan the file line by line
 	for scanner.Scan() {
 		lineNum++
 		line := scanner.Text()
+		// If the content contains pattern not to match, skip the line
 		if len(ia) > 0 && matchArray(line, ia) {
 			continue
 		}
+		// If the content contains pattern to match, save the line information
 		if matchArray(line, cra) {
 			greps = append(greps, grep{lineNum, line})
 		}
 	}
+	// If the target file matched at least one pattern, save the target file information
 	if len(greps) > 0 {
 		grepResult := grepStruct{file, greps}
 		if filenameOnlyFlag {
@@ -206,6 +246,7 @@ func grepWorkConc(
 	}
 }
 
+// Actually grepping function for normal searching
 func grepWork(
 	file string,
 	cra []regexp.Regexp,
@@ -213,6 +254,7 @@ func grepWork(
 	encoding encoding.Encoding,
 	filenameOnlyFlag bool,
 ) {
+	// Open and read the target file
 	fp, err := os.Open(file)
 	if err != nil {
 		return
@@ -225,16 +267,20 @@ func grepWork(
 	// scanner.Buffer(make([]byte, maxBufSize), maxBufSize)
 	lineNum := 0
 	var greps []grep
+	// Scan the file line by line
 	for scanner.Scan() {
 		lineNum++
 		line := scanner.Text()
+		// If the content contains pattern not to match, skip the line
 		if len(ia) > 0 && matchArray(line, ia) {
 			continue
 		}
+		// If the content contains pattern to match, save the line information
 		if matchArray(line, cra) {
 			greps = append(greps, grep{lineNum, line})
 		}
 	}
+	// If the target file matched at least one pattern, save the target file information
 	if len(greps) > 0 {
 		grepResult := grepStruct{file, greps}
 		if filenameOnlyFlag {
